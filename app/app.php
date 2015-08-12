eb95aac69c1f21874b73408ad727f17659dca7db
Jeff Austin <jeffaustin81@gmail.com>
2015-08-11 20:00:39 -0700

    Added comments to class Anagram

--- app/app.php
+++ app/app.php
@@ -1,10 +1,14 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Anagram.php";
+
     use Symfony\Component\Debug\Debug;
     Debug::enable();
+
     $app = new Silex\Application();
+
     $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use($app) {
--- src/Anagram.php
+++ src/Anagram.php
@@ -4,14 +4,20 @@
     {
         function findAnagrams($input, $words_to_check)
         {
+            // Takes input and splits string into an array of letters
+            // then sorts those letters into alphabetical order
             $input_array = str_split($input);
             sort($input_array);
 
+            // if input is only one letter reurns "No anagrams"
             if (strlen($input) == 1)
             {
                 return "No anagrams";
             }
 
+            // Takes words to check and explodes the input into an array
+            // of the words entered and then we create an empty array to
+            // store evaluated words
             $array_words_to_check = explode(" ", $words_to_check);
             $anagram_array = array();
 
